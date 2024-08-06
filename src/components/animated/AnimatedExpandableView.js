ab228cf691aa82de9ed46146e0dd07242e753d21
JoshiParSa <joshiparsa@gmail.com>
2024-08-06 14:24:31 -0500

    Change: Optimization in animation and use of animated values

--- src/components/animated/AnimatedExpandableView.js
+++ src/components/animated/AnimatedExpandableView.js
@@ -3,63 +3,42 @@ import { Animated, Easing } from "react-native";
 import PropTypes from "prop-types";
 import { useRef, useEffect } from "react";
 
+/**
+ * Visibility is boolean, if visibility is true the children will be displayed
+ * @param {*} props 
+ * @returns AnimatedExpandableView
+ */
 const AnimatedExpandableView = (props) => {
   const { visibility, style, children, duration, easing, minSize, maxSize } =
     props;
 
-  const animatedMaxHeight = useRef(
-    new Animated.Value(visibility ? 0 : 1)
-  ).current;
-
-  const animatedMaxWidth = useRef(
-    new Animated.Value(visibility ? 0 : 1)
-  ).current;
-
-  const animatedOpacity = useRef(
-    new Animated.Value(visibility ? 0 : 1)
-  ).current;
-
-  const expandibleStyle = {
-    maxHeight: animatedMaxHeight.interpolate({
-      inputRange: [0, 1],
-      outputRange: [minSize[1] ?? 0, maxSize[1] ?? 4000],
-      extrapolate: "clamp",
-    }),
-    maxWidth: animatedMaxHeight.interpolate({
-      inputRange: [0, 1],
-      outputRange: [minSize[0] ?? 0, maxSize[0] ?? 4000],
-      extrapolate: "clamp",
-    }),
-    opacity: animatedOpacity
-  };
+  const animated = useRef(new Animated.Value(visibility ? 1 : 0)).current;
 
   useEffect(() => {
     animation();
   }, [visibility]);
 
   const animation = () => {
-    Animated.parallel(
-      [
-        Animated.timing(animatedMaxHeight, {
-          toValue: visibility ? 1 : 0,
-          duration: duration || 250,
-          easing: easing || Easing.linear,
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedMaxWidth, {
-          toValue: visibility ? 1 : 0,
-          duration: duration || 250,
-          easing: easing || Easing.linear,
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedOpacity, {
-          toValue: visibility ? 1 : 0,
-          duration: duration || 250,
-          easing: easing || Easing.linear,
-          useNativeDriver: false,
-        })
-      ]
-    ).start();
+    Animated.timing(animated, {
+      toValue: visibility ? 1 : 0,
+      duration: duration || 250,
+      easing: easing || Easing.linear,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const expandibleStyle = {
+    maxHeight: animated.interpolate({
+      inputRange: [0, 1],
+      outputRange: [minSize[1] ?? 0, maxSize[1] ?? 4000],
+      extrapolate: "clamp",
+    }),
+    maxWidth: animated.interpolate({
+      inputRange: [0, 1],
+      outputRange: [minSize[0] ?? 0, maxSize[0] ?? 4000],
+      extrapolate: "clamp",
+    }),
+    opacity: animated,
   };
 
   return (
