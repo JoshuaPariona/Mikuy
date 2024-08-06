955b67559454c1ceb90559a96282d673e92d312b
JoshiParSa <joshiparsa@gmail.com>
2024-08-06 14:18:36 -0500

    File: Paths store the all paths used inthe app

---
+++ src/utils/paths.js
@@ -0,0 +1,12 @@
+export default class Paths  {
+  static requireIconCategory = {
+    1: require("../../assets/icons/icon_category_1.png"),
+    2: require("../../assets/icons/icon_category_2.png"),
+    3: require("../../assets/icons/icon_category_3.png"),
+    4: require("../../assets/icons/icon_category_4.png"),
+    5: require("../../assets/icons/icon_category_5.png"),
+    6: require("../../assets/icons/icon_category_6.png"),
+    7: require("../../assets/icons/icon_category_7.png"),
+    8: require("../../assets/icons/icon_category_8.png"),
+  }
+}
